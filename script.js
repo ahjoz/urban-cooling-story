49f19b26352dc30546523e96a7e8bace66fda63a
ahjoz <jozhu786@gmail.com>
2026-08-14 18:44:02 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const flipCards = document.querySelectorAll(".flip-card");
+
+flipCards.forEach(card => {
+
+  const buttons = card.querySelectorAll(".flip-button");
+
+  buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+      card.classList.toggle("is-flipped");
+
+    });
+
+  });
+
+});
